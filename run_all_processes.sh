9eb9a79eff84424016286125e6a9ebf418230c23
Mohammed Ajil <mohammed@ajil.ch>
2020-03-18 12:43:06 +0100

    run jobs in docker containers in parallel

--- run_all_processes.sh
+++ run_all_processes.sh
@@ -1,8 +1,9 @@
-export LOGLEVEL="INFO"
-export DB_CONN="postgresql://wheretolive:wheretolive@localhost:5432/wheretolive"
-python -m wheretolive.processes.init_db
-python -m wheretolive.processes.towns_to_db
-python -m wheretolive.processes.route_distances_to_db
-python -m wheretolive.processes.tax_rates_to_db
-python -m wheretolive.processes.health_insurance_to_db
+# export LOGLEVEL="INFO"
+# export DB_CONN="postgresql://wheretolive:wheretolive@localhost:5432/wheretolive"
+DOCKER_PREFIX='docker run -d --rm -e "LOGLEVEL=INFO" -e "DB_CONN=postgresql://wheretolive:wheretolive@localhost:5432/wheretolive" wheretolive'
+$DOCKER_PREFIX python -m wheretolive.processes.init_db
+$DOCKER_PREFIX python -m wheretolive.processes.towns_to_db
+$DOCKER_PREFIX python -m wheretolive.processes.route_distances_to_db
+$DOCKER_PREFIX python -m wheretolive.processes.tax_rates_to_db
+$DOCKER_PREFIX python -m wheretolive.processes.health_insurance_to_db
 
