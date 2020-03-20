ff6a4b62f036f0cb5f34daccec7b7edc23916f57
Mohammed Ajil <mohammed@ajil.ch>
2020-03-20 19:44:25 +0100

    simplify run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -1,2 +1,1 @@
-DOCKER_PREFIX='docker run --name '$1' -d --network container:db -e "LOGLEVEL=INFO" -e "DB_CONN=postgresql://wheretolive:wheretolive@db:5432/wheretolive" -e "MAPS_API_KEY=AIzaSyBzWe16UIfLa_5u1SMiRACTcXOr-K0zCrU" wheretolive'
-/bin/bash -c "$DOCKER_PREFIX -m wheretolive.processes.$1"
+/bin/bash -c "docker run --name $1 -d --network container:db -e 'LOGLEVEL=INFO' -e 'DB_CONN=postgresql://wheretolive:wheretolive@db:5432/wheretolive' wheretolive -m wheretolive.jobs.$1"
