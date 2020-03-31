a4936c8462f33410b0d95a6a6478e24af248230d
Mohammed Ajil <mohammed@ajil.ch>
2020-03-31 10:55:29 +0200

    added some todos

---
+++ wheretolive/aggregators/_tax_rate_aggregator.py
@@ -0,0 +1,5 @@
+# TODO: Setup Model
+# TODO: Train model
+# TODO: Predict tax rate for up to 5 children
+# TODO: Create new table (PredictedTaxRate or something)
+# TODO: Add column indicating whether it is an exact number or a prediction
