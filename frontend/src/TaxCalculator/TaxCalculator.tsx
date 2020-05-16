165f70c11c262b1fe4022353bc782507417e4a6a
Mohammed Ajil <mohammed@ajil.ch>
2020-05-16 21:44:39 +0200

    forgot to save -.-

--- frontend/src/TaxCalculator/TaxCalculator.tsx
+++ frontend/src/TaxCalculator/TaxCalculator.tsx
@@ -44,7 +44,7 @@ class TaxCalculator extends React.Component<Props, State> {
       }),
     };
 
-    fetch("http://" + process.env.REACT_APP_BACKEND_HOST +":" + process.env.REACT_APP_BACKEND_PORT +"/tax_calculator/", requestOptions)
+    fetch(process.env.REACT_APP_BACKEND_PROTOCOL + "://" + process.env.REACT_APP_BACKEND_HOST +":" + process.env.REACT_APP_BACKEND_PORT +"/tax_calculator/", requestOptions)
       .then((response) => response.json())
       .then((data) => {
         this.setState({
