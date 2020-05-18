a5374dac5cab37289fa9ac081716fb63e5bcb2bd
Mohammed Ajil <mohammed@ajil.ch>
2020-05-18 14:33:52 +0200

    remove unused route

--- backend/blueprints/towns.py
+++ backend/blueprints/towns.py
@@ -1,23 +1,8 @@
-import json
+from flask import Blueprint, jsonify
 
-from flask import Blueprint, jsonify, request
-from plotly.utils import PlotlyJSONEncoder
-
-from ..services import TownsAnalysisService, TownService
+from ..services import TownService
 
 towns_bp = Blueprint("towns", __name__, template_folder="templates")
-
-
-@towns_bp.route("/", methods=["POST"])
-def show():
-    service = TownsAnalysisService()
-    analysis, figure_data = service.analyze(request.get_json())
-    return jsonify(
-        {
-            "analysis": analysis,
-            "figure_data": json.dumps(figure_data, cls=PlotlyJSONEncoder),
-        }
-    )
 
 
 @towns_bp.route("/typeahead", methods=["GET"])
